f50a5fb849e3e680240498938d5c39c81f740866
bojo <wzxutao@gmail.com>
2020-03-27 23:12:33 +0000

    rename the sketch sketch to Arduino_HiveMQ_connector, which is what it should be

--- Arduino_HiveMQ_connector/Arduino_HiveMQ_connector.ino
+++ Arduino_HiveMQ_connector/Arduino_HiveMQ_connector.ino
@@ -1,21 +1,24 @@
-#include <M5Stack.h>
-#include <WiFi.h>
-#include <esp_wifi.h>
-WiFiClient wifi_client;
+#include <SPI.h>
+#include <WiFiNINA.h>
 
-// PubSubClient external library.
+WiFiClient wifi_client;
 #include <PubSubClient.h>
 PubSubClient ps_client( wifi_client );
 
+//Default: 128bytes, which is too small.
+//change the header file there: C:\Users\?\Documents\Arduino\libraries\PubSubClient\src
+//#define MQTT_MAX_PACKET_SIZE 8192
 
-// Extra, created by SEGP team
-#include "Timer.h"
+
+
+#define SERIAL_DELIMITER '#'
 
 // Wifi settings
-uint8_t guestMacAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-const char* wifi_ssid = "LAPTOP-3MHBNCLF 4177";                 // Set name of Wifi Network
-const char* wifi_password = "malvinas";                      // No password for UoB Guest
+char wifi_ssid[] = "LAPTOP-3MHBNCLF 4177";   
+char wifi_password[] = "malvinas";   
 
+// Serial settings
+const int serial1_timeout {300}; // in milliseconds
 
 // MQTT Settings
 const char* MQTT_clientname = "barrier"; // Make up a short name
@@ -26,92 +29,44 @@ const char* MQTT_pub_topic = "m5comm"; // You might want to create your own
 const char* server = "broker.mqttdashboard.com";
 const int port = 1883;
 
+void publishFromSerial1(){
+  int byteCount = Serial1.available();
+  if(byteCount <=0){return;}
+  Serial1.find(SERIAL_DELIMITER);
+  //char *byteBuffer { new char[byteCount + 1] {} }; 
+  String serial1Read = Serial1.readStringUntil(SERIAL_DELIMITER);
+  
+  if(ps_client.connected()){
+    //Serial.write(byteBuffer, byteCount);
+    //ps_client.subscribe(MQTT_pub_topic);
+    //ps_client.write(byteBuffer, byteCount);
+    //Serial.println(byteBuffer);
+    //ps_client.publish(MQTT_pub_topic, byteBuffer);
+    Serial.println(serial1Read);
+    publishMessage(serial1Read);
+  }else{
+    Serial1.println("Can't publish message: Not connected to MQTT :( ");
+    Serial.println("Can't publish message: Not connected to MQTT :( ");
+  }
+  //delete[] byteBuffer;
+}
 
-// Instance of a Timer class, which allows us
-// a basic task scheduling of some code.  See
-// it used in Loop().
-// See Timer.h for more details.
-// Argument = millisecond period to schedule
-// task.  Here, 2 seconds.
-Timer publishing_timer(2000);
-
-
-// Arduino setup
-void setup() {
-    M5.begin();
-    M5.Power.begin();
-    M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.setCursor( 10, 10 );
-    M5.Lcd.setTextColor( WHITE );
-    M5.Lcd.println("Reset, connecting...");
-
-
-    // Setup a serial port, good for debugging.
-    // You can view data with the Arduino IDE
-    // serial monitor.
-    Serial.begin(115200);
-    delay(10);
-    Serial.println("*** RESET ***\n");
-
+void setupWifi(){
+  Serial1.println("Connecting to wifi.");
+  do{
     WiFi.begin(wifi_ssid, wifi_password);
-    //setupWifiWithPassword();
+    const int waitSectionCount = 10;
+    for(int i=0; i<waitSectionCount; i++){
+      if(WiFi.status() == WL_CONNECTED){
+        break;
+      }
+      Serial1.println("unable to connect to wifi. Still trying...");
+      delay(500);
+    }   
+  }while(WiFi.status() != WL_CONNECTED);
 
-    // Sets up a connection to hiveMQ.
-    // Sets up a call back function to run
-    // which checks for new messages.
-    setupMQTT();
-
+  Serial1.println("wifi connected.");
 }
-
-
-// Arduino loop
-void loop() {
-
-  // Leave this code here.  It checks that you are
-  // still connected, and performs an update of itself.
-  if (!ps_client.connected()) {
-    reconnect();
-  }
-  ps_client.loop();
-
-
-  // This is an example of using our timer class to
-  // publish a message every 2000 milliseconds, as
-  // set when we initalised the class above.
-  if( publishing_timer.isReady() ) {
-
-      // Prepare a string to send.
-      // Here we include millis() so that we can
-      // tell when new messages are arrive in hiveMQ
-      String new_string = "hello?";
-      new_string += millis();
-      publishMessage( new_string );
-
-      // Remember to reset your timer when you have
-      // used it. This starts the clock again.
-      publishing_timer.reset();
-  }
-
-
-
-
-  // Just incase we print so much text we run off the
-  // screen!  Clear screen, set cursor back to the top.
-  if( M5.Lcd.getCursorY() > M5.Lcd.height() ) {
-    M5.Lcd.fillScreen( BLACK );
-    M5.Lcd.setCursor( 0, 10 );
-  }
-}
-
-
-
-
-
-/*******************************************************************************************
- *
- * Helper functions after this...
- *
- ******************************************************************************************/
 
 
 // Use this function to publish a message.  It currently
@@ -133,23 +88,23 @@ void publishMessage( String message ) {
     if( message.length() > 0 ) {
 
       // Convert to char array
-      char msg[ message.length() ];
+      char *msg {new char[message.length()] {}};
       message.toCharArray( msg, message.length() );
 
-      M5.Lcd.print(">> Tx: ");
-      M5.Lcd.println( message );
+      Serial.print(">> Tx: ");
+      Serial.println( msg );
 
       // Send
-      ps_client.publish( MQTT_pub_topic, msg );
+      ps_client.publish( MQTT_pub_topic, msg);
+      delete[] msg;
     }
 
   } else {
     Serial.println("Can't publish message: Not connected to MQTT :( ");
 
   }
-
-
 }
+
 
 // This is where we pick up messages from the MQTT broker.
 // This function is called automatically when a message is
@@ -162,79 +117,35 @@ void publishMessage( String message ) {
 
 void callback(char* topic, byte* payload, unsigned int length) {
 
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  Serial1.print("Message arrived [");
+  Serial1.print(topic);
+  Serial1.print("] ");
 
   String in_str = "";
 
   // Copy chars to a String for convenience.
-  // Also prints to USB serial for debugging
+  // Also prints to USB Serial1 for debugging
   for (int i=0;i<length;i++) {
     in_str += (char)payload[i];
-    Serial.print((char)payload[i]);
+    Serial1.print((char)payload[i]);
   }
-  Serial.println();
+  Serial1.println();
 
-  M5.Lcd.print(" << Rx: " );
-  M5.Lcd.println( in_str );
-
-
+  Serial1.print(" << Rx: " );
+  Serial1.println( in_str );
 }
-
-
-
-/*******************************************************************************************
- *
- *
- * You shouldn't need to change any code below this point!
- *
- *
- *
- ******************************************************************************************/
-
 
 void setupMQTT() {
     ps_client.setServer(server, port);
     ps_client.setCallback(callback);
 }
 
-void setupWifi() {
-
-    byte mac[6];
-    Serial.println("Original MAC address: " + String(WiFi.macAddress()));
-    esp_base_mac_addr_set(guestMacAddress);
-    Serial.println("Borrowed MAC address: " + String(WiFi.macAddress()));
-
-    Serial.println("Connecting to network: " + String(wifi_ssid));
-    WiFi.begin(wifi_ssid);
-    while (WiFi.status() != WL_CONNECTED) delay(500);
-    Serial.println("IP address allocated: " + String(WiFi.localIP()));
-}
-
-void setupWifiWithPassword( ) {
-
-    byte mac[6];
-    Serial.println("Original MAC address: " + String(WiFi.macAddress()));
-    esp_base_mac_addr_set(guestMacAddress);
-    Serial.println("Borrowed MAC address: " + String(WiFi.macAddress()));
-
-    Serial.println("Connecting to network: " + String(wifi_ssid));
-    WiFi.begin(wifi_ssid, wifi_password);
-
-    while (WiFi.status() != WL_CONNECTED) delay(500);
-    Serial.println("IP address allocated: " + String(WiFi.localIP()));
-
-}
-
-
-
 void reconnect() {
 
   // Loop until we're reconnected
   while (!ps_client.connected()) {
 
-    Serial.print("Attempting MQTT connection...");
+    Serial1.print("Attempting MQTT connection...");
 
     // Attempt to connect
     // Sometimes a connection with HiveMQ is refused
@@ -243,30 +154,30 @@ void reconnect() {
     // every time we need to reconnect.
     String new_id = generateID();
 
-    Serial.print("connecting with ID ");
-    Serial.println( new_id );
+    Serial1.print("connecting with ID ");
+    Serial1.println( new_id );
 
     char id_array[ (int)new_id.length() ];
     new_id.toCharArray(id_array, sizeof( id_array ) );
 
     if (ps_client.connect( id_array ) ) {
-      Serial.println("connected");
+      Serial1.println("connected");
 
       // Once connected, publish an announcement...
       ps_client.publish( MQTT_pub_topic, "reconnected");
       // ... and resubscribe
       ps_client.subscribe( MQTT_sub_topic );
     } else {
-      M5.Lcd.println(" - Coudn't connect to HiveMQ :(");
-      M5.Lcd.println("   Trying again.");
-      Serial.print("failed, rc=");
-      Serial.print(ps_client.state());
-      Serial.println(" try again in 5 seconds");
+      Serial1.println(" - Coudn't connect to HiveMQ :(");
+      Serial1.println("   Trying again.");
+      Serial1.print("failed, rc=");
+      Serial1.print(ps_client.state());
+      Serial1.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
-  M5.Lcd.println(" - Success!  Connected to HiveMQ\n\n");
+  Serial1.println(" - Success!  Connected to HiveMQ\n\n");
 }
 
 String generateID() {
@@ -275,4 +186,24 @@ String generateID() {
   id_str += random(0,9999);
 
   return id_str;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  //Serial1.setTimeout(serial1_timeout);
+  setupWifi();
+  setupMQTT();
+}
+
+void loop() {
+  // Leave this code here.  It checks that you are
+  // still connected, and performs an update of itself.
+  if (!ps_client.connected()) {
+    reconnect();
+  }
+  ps_client.loop();
+
+  publishFromSerial1();
+
 }
