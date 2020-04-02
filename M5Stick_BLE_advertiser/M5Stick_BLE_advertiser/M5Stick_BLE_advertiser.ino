db3fb269efe30d4ec539f1ec4ed2f80fb767c2e4
bojo <wzxutao@gmail.com>
2020-04-02 19:15:59 +0100

    add button control for BLE advertising state(ON/OFF)

--- M5Stick_BLE_advertiser/M5Stick_BLE_advertiser/M5Stick_BLE_advertiser.ino
+++ M5Stick_BLE_advertiser/M5Stick_BLE_advertiser/M5Stick_BLE_advertiser.ino
@@ -1,17 +1,11 @@
+#include <M5StickC.h>
 #include "BLEDevice.h"
-#define BtnPin 35
-#define BLE_DEVICE_NAME "M5Stick" // BLE name of this device
 
+#define BLE_DEVICE_NAME "M5StickC"
 BLEAdvertising* pBLEAdvertising;
-void setup() {
-  // put your setup code here, to run once:
-  BLEDevice::init(BLE_DEVICE_NAME);
-  pBLEAdvertising = BLEDevice::getAdvertising();
-  pBLEAdvertising->start();
-  Serial.begin(115200);
 
+enum M5BLEState {ON, OFF} m5BLEState { OFF };
 
-}
 
 // send our own BLE address to serial
 void sendBLEAddrToSerial(){
@@ -19,9 +13,39 @@ void sendBLEAddrToSerial(){
     Serial.println(BLEaddr.c_str());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  sendBLEAddrToSerial();
-  delay(300);
+inline void handleButtonInterrupt(){
+  M5.update();
+  if(M5.BtnA.wasPressed()){
+    switchBLEAdvertisingState();
+  }
+}
 
+void switchBLEAdvertisingState(){
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  if(m5BLEState == OFF){
+    pBLEAdvertising->start();
+    m5BLEState = ON;
+    M5.Lcd.println("ON");
+  }else{
+    pBLEAdvertising->stop();
+    m5BLEState = OFF;
+    M5.Lcd.println("OFF");
+  }
+  sendBLEAddrToSerial();
+}
+
+void setup() {
+  M5.begin();
+  Serial.begin(115200);
+  BLEDevice::init(BLE_DEVICE_NAME);
+
+  pBLEAdvertising = BLEDevice::getAdvertising();
+  M5.Lcd.setTextSize(5);
+  switchBLEAdvertisingState();
+}
+
+void loop() {
+  handleButtonInterrupt();
+  delay(300);
 }
